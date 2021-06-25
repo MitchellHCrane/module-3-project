e9db1095bf08f7c81868cf67411e23e4daeeea6b
Mitchell Crane <mitchellhcrane@gmail.com>
2021-06-25 10:26:41 -0600

    update with partial extra credit

---
+++ neighborhood.js
@@ -0,0 +1,9 @@
+const resArray = document.querySelector('#restaurants')
+const random = Math.floor(Math.random() * resArray.length);
+
+Math.floor(Math.random() * resArray.length);
+console.log(random, randomRes[random]);
+
+console.log(resArray)
+
+//Couldn't quite finish. Got stuck
--- profile.js
+++ profile.js
@@ -1,19 +1,17 @@
-
-
-
 const favColor = document.querySelector("#color");
 const favPlace = document.querySelector("#place");
 const favRitual = document.querySelector("#ritual");
 
-
 document.addEventListener("click", () => {
-    alert("My Favorite Color is: Red");
+  alert("My Favorite Color is: Red");
 });
 
 document.addEventListener("click", () => {
-    alert("My Favorite Place is: Hawaii");
+  alert("My Favorite Place is: Hawaii");
 });
 
 document.addEventListener("click", () => {
-    alert("My Favorite Ritual is: Exercise");
+  alert("My Favorite Ritual is: Exercise");
 });
+
+//Not sure how to seperate these so that they don't run all at once. Got stuck.
