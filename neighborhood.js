ff88752ea5fd7a77490ed729e5a5f49b784972ab
Mitchell Crane <mitchellhcrane@gmail.com>
2021-06-25 12:59:50 -0600

    reviewUpdate

--- neighborhood.js
+++ neighborhood.js
@@ -1,9 +1,23 @@
-const resArray = document.querySelector('#restaurants')
-const random = Math.floor(Math.random() * resArray.length);
+// const resArray = document.querySelector('#restaurants')
+// const random = Math.floor(Math.random() * resArray.length);
 
-Math.floor(Math.random() * resArray.length);
-console.log(random, randomRes[random]);
+// Math.floor(Math.random() * resArray.length);
+// console.log(random, randomRes[random]);
 
-console.log(resArray)
+// console.log(resArray)
 
 //Couldn't quite finish. Got stuck
+
+const restaurantRes = document.getElementById('restaurant-button')
+
+const restaurantList = document.querySelector('.random-restaurant')
+
+const restaurantArr = ['Tadka Cuisine of India', 'El Tapatio', 'Daley Freez']
+
+const getRestaurant = () => {
+    const random = Math.floor(Math.random() = randomArr.length)
+    const restaurantP = document.createElement('p')
+    restaurantP.textContent= restaurantArr[random]
+    restaurantList.appendChild(restaurantP)
+}
+randomButton.addEventListener('click', getRestaurant)
--- profile.js
+++ profile.js
@@ -2,16 +2,25 @@ const favColor = document.querySelector("#color");
 const favPlace = document.querySelector("#place");
 const favRitual = document.querySelector("#ritual");
 
-document.addEventListener("click", () => {
-  alert("My Favorite Color is: Red");
-});
+favColor.addEventListener("click", () => alert("My Favorite Color is: Red"));
 
-document.addEventListener("click", () => {
-  alert("My Favorite Place is: Hawaii");
-});
+favPlace.addEventListener("click", () => alert("My Favorite Place is: Hawaii"));
 
-document.addEventListener("click", () => {
-  alert("My Favorite Ritual is: Exercise");
-});
+favRitual.addEventListener("click", () =>
+  alert("My Favorite Ritual is: Exercise")
+);
 
-//Not sure how to seperate these so that they don't run all at once. Got stuck.
+// document.addEventListener("click", () => {
+//   alert("My Favorite Color is: Red");
+// });
+
+// document.addEventListener("click", () => {
+//   alert("My Favorite Place is: Hawaii");
+// });
+
+// document.addEventListener("click", () => {
+//   alert("My Favorite Ritual is: Exercise");
+// });
+
+//Above commented section is wrong because document. runs on the whole document instead of just favColor/Place/Ritual
+
